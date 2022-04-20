d9bc32021a2e5bbee9104df9b20a82d1ebacfff7
Kirill Leontev <leontiev.k@gmail.com>
2022-04-20 23:16:45 +0700

    0206 use util.py

--- py/e_0206_reverse_linked_list.py
+++ py/e_0206_reverse_linked_list.py
@@ -1,15 +1,8 @@
 # https://leetcode.com/problems/reverse-linked-list
-from typing import Optional, List
+from typing import Optional
 import unittest
 
-
-class ListNode:
-    def __init__(self, val=0, next: 'ListNode' = None):
-        self.val = val
-        self.next = next
-
-    def __str__(self):
-        return f'ListNode(val={self.val}, next={self.next})'
+from util import ListNode, listnode_to_list, list_to_listnode
 
 
 class Solution:
@@ -34,38 +27,24 @@ class Solution:
         return prev_node
 
 
-def _list_node_to_list(head: Optional[ListNode]) -> List[int]:
-    if not head:
-        return []
-
-    if not head.next:
-        return [head.val]
-
-    node = head
-    result = []
-    while node:
-        result.append(node.val)
-        node = node.next
-
-    return result
-
-
 class TestSolution(unittest.TestCase):
     def test_solution(self):
-        solution = Solution()
-
         tests = [
-            ListNode(0, ListNode(1, ListNode(2, ListNode(3, ListNode(4))))),
-            ListNode(0, ListNode(1, ListNode(2, ListNode(1, ListNode(0))))),
-            ListNode(0, ListNode(1)),
-            ListNode(0),
-            ListNode(0, ListNode(1, ListNode(0))),
+            [0, 1, 2, 3, 4],
+            [0, 1, 2, 1, 0],
+            [0, 1],
+            [0],
+            [0, 1, 0],
             None
         ]
 
         for test in tests:
             with self.subTest(f'test={test}'):
                 self.assertEqual(
-                    list(reversed(_list_node_to_list(test))),
-                    _list_node_to_list(solution.reverseList(test))
+                    listnode_to_list(
+                        Solution().reverseList(
+                            list_to_listnode(test)
+                        )
+                    ),
+                    list(reversed(test or [])),
                 )
