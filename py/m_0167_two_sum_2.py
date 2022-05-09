fe7950ec858bd8c63735ed6fe7913f73ba437b14
kleontev <kleontev@users.noreply.github.com>
2022-05-09 09:20:20 +0700

    0167 binary_search method from util

--- py/m_0167_two_sum_2.py
+++ py/m_0167_two_sum_2.py
@@ -5,34 +5,6 @@ import unittest
 from util import binary_search
 
 
-def binary_search(nums: List[int], target: int, first: bool = True) -> int:
-    result = -1
-
-    if not nums:
-        return result
-
-    left = 0
-    right = len(nums) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if nums[mid] == target:
-            result = mid
-            if first:
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        elif nums[mid] > target:
-            right = mid - 1
-
-        else:
-            left = mid + 1
-
-    return result
-
-
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         p1 = 0
