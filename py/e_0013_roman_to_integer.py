7f0df8039b8d41c839b7a5eceb96ee843af5b164
kleontev <kleontev@users.noreply.github.com>
2022-06-20 15:06:41 +0700

    e_0013_roman_to_integer

--- py/e_0013_roman_to_integer.py
+++ py/e_0013_roman_to_integer.py
@@ -1,10 +1,41 @@
+# https://leetcode.com/problems/roman-to-integer/
 from dataclasses import dataclass
 import unittest
 
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        assert len(s) > 0
+
+        NUMBERS = dict(
+            I=1,
+            V=5,
+            X=10,
+            L=50,
+            C=100,
+            D=500,
+            M=1000
+        )
+
+        # if len(s) == 1:
+            # return NUMBERS[s[0]]
+
+        result = 0
+
+        prev = None
+
+        for c in reversed(s):
+            if NUMBERS[c] < NUMBERS.get(prev, -1):
+                result -= NUMBERS[c]
+            else:
+                result += NUMBERS[c]
+            prev = c
+        return result
+
+
+# LVIII
+# 1) result += 1
+# 2) result += 1
 
 
 @dataclass
@@ -14,9 +45,11 @@ class TestCase:
 
 
 TESTS = [
+    TestCase('I', 1),
+    TestCase('M', 1000),
     TestCase('III', 3),
     TestCase('LVIII', 58),
-    TestCase('MCMXCIV', 1994),
+    TestCase('MCMXCIV', 1994), # MCMXCIV 5-1+50-10+1000
 ]
 
 
