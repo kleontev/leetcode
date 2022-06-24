2b2f5fd6700ae7bbce6e739c76cfa5c485c5cca3
kleontev <kleontev@users.noreply.github.com>
2022-06-24 19:03:31 +0700

    e_0068_sqrt

---
+++ py/e_0068_sqrt.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/add-binary/
+import unittest
+import math
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+
+        prev_guess = x
+        guess = 0.5 * (prev_guess + x / prev_guess)
+
+        while abs(guess - prev_guess) >= 1:
+            prev_guess = guess
+            guess = 0.5 * (prev_guess + x / prev_guess)
+            
+
+        return int(guess)
+
+
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        for t in range(1_000_000):
+            self.assertEqual(
+                Solution().mySqrt(t),
+                int(t ** 0.5)
+            )
